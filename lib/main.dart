893d85e70efa254713da485e6c722c3ad3e49ae5
muhammad-hamza-shahid <50265613+muhammad-hamza-shahid@users.noreply.github.com>
2020-07-28 23:46:40 +0500

    started MI Buisness Card

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,11 @@ class myApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: <Widget>[
-              Container(
-                width: 100,
-                color: Colors.red,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                ],
-              ),
-              Container(
-                width: 100,
-                color: Colors.blue,
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('images/upload.jpg'),
               ),
             ],
           ),
