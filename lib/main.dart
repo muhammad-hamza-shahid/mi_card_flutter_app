4d0e4243c1132cdbfb5515ae6dec4dbd06e04fa9
muhammad-hamza-shahid <50265613+muhammad-hamza-shahid@users.noreply.github.com>
2020-07-28 23:03:51 +0500

    implemented Container widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,9 +9,19 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      color: Colors.teal,
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.teal,
           title: Text('Muhammad Hamza Shahid'),
+        ),
+        body: Container(
+          color: Colors.red,
+          width: 100,
+          padding: EdgeInsets.all(10),
+          height: 50,
+          margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+          child: Text('A container can have only one child'),
         ),
       ),
     );
