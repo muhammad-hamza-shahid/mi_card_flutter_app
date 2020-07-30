d49f878411df2839cd857f1326dda0cb3fbf899b
muhammad-hamza-shahid <50265613+muhammad-hamza-shahid@users.noreply.github.com>
2020-07-30 17:04:57 +0500

    added font and created contact cards

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,79 @@ class myApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 60,
-                backgroundImage: AssetImage('images/upload.jpg'),
-              ),
-              Text(
-                'Muhammad Hamza',
-                style: TextStyle(
-                    fontSize: 30,
-                    fontFamily: 'Pacifico',
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-            ],
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/mi.jpg'),
+                ),
+                Text(
+                  'Muhammad Hamza',
+                  style: TextStyle(
+                      fontSize: 35,
+                      fontFamily: 'Pacifico',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Text(
+                  'ANDROID & FLUTTER DEVELOPER',
+                  style: TextStyle(
+                      fontFamily: 'fonts/SourceSan',
+                      fontSize: 22,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal.shade700,
+                      ),
+                      SizedBox(
+                        height: 0,
+                        width: 10,
+                      ),
+                      Text(
+                        '+92343 8559089',
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontFamily: 'SourceSan',
+                            color: Colors.teal.shade900),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal.shade700,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'mr.hamza.shahid@gmail.com',
+                        style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSan'),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
